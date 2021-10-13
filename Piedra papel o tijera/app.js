ffead88905428189f048341517e645a0c4348ba4
Wildecity <81671271+Wildecity@users.noreply.github.com>
2021-10-13 15:35:53 -0300

    User Interface v1

--- Piedra papel o tijera/app.js
+++ Piedra papel o tijera/app.js
@@ -1,11 +1,42 @@
+const playerDisplay = document.querySelector("#playerScore");
+const computerDisplay = document.querySelector("#computerScore");
+const winnerDisplay = document.querySelector("#winner");
+const buttons = document.querySelectorAll("button");
+const playerChoice = document.querySelector("#playerSelection");
+const computerChoice = document.querySelector("#computerSelection");
+const roundWinnerDisplay = document.querySelector("#roundWinner");
+
 let computerSelection = "";
 let playerSelection = "";
 let playerScore = 0;
 let computerScore = 0;
+let roundWinner = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerSelection = button.id;
+    computerSelection = computerPlay();
+    playerChoice.textContent = playerSelection;
+    computerChoice.textContent = computerSelection;
+    playRound(playerSelection, computerSelection);
+    playerDisplay.textContent = playerScore;
+    computerDisplay.textContent = computerScore;
+    if (playerScore == 5 || computerScore == 5) {
+      showWinner();
+      disableBtns();
+    }
+  });
+});
 
 function resetScore() {
   playerScore = 0;
   computerScore = 0;
+}
+
+function disableBtns() {
+  buttons.forEach((button) => {
+    button.classList.add("disabled");
+  });
 }
 
 function computerPlay() {
@@ -22,62 +53,60 @@ function computerPlay() {
 
 function playRound(playerChoice, computerChoice) {
   if (computerChoice === playerChoice) {
-    return `computadora eligio ${computerChoice} y jugador eligio ${playerChoice}. Es un empate!`;
+    roundWinner = "nobody";
+  } else {
+    switch (computerChoice) {
+      case "rock":
+        if (playerChoice === "paper") {
+          playerScore++;
+          roundWinner = "player";
+        } else if (playerChoice === "scissors") {
+          computerScore++;
+          roundWinner = "computer";
+        }
+        break;
+      case "paper":
+        if (playerChoice === "scissors") {
+          playerScore++;
+          roundWinner = "player";
+        } else if (playerChoice === "rock") {
+          computerScore++;
+          roundWinner = "computer";
+        }
+        break;
+      case "scissors":
+        if (playerChoice === "rock") {
+          playerScore++;
+          roundWinner = "player";
+        } else if (playerChoice === "paper") {
+          computerScore++;
+          roundWinner = "computer";
+        }
+    }
   }
-  switch (computerChoice) {
-    case "rock":
-      if (playerChoice === "paper") {
-        playerScore++;
-        return `computadora eligio ${computerChoice} y jugador eligio ${playerChoice}. Gana el jugador!`;
-      } else if (playerChoice === "scissors") {
-        computerScore++;
-        return `computadora eligio ${computerChoice} y jugador eligio ${playerChoice} Gana la computadora`;
-      }
-      break;
-    case "paper":
-      if (playerChoice === "scissors") {
-        playerScore++;
-        return `computadora eligio ${computerChoice} y jugador eligio ${playerChoice}. Gana el jugador!`;
-      } else if (playerChoice === "rock") {
-        computerScore++;
-        return `computadora eligio ${computerChoice} y jugador eligio ${playerChoice}. Gana la computadora!`;
-      }
-      break;
-    case "scissors":
-      if (playerChoice === "rock") {
-        playerScore++;
-        return `computadora eligio ${computerChoice} y jugador eligio ${playerChoice}. Gana el jugador!`;
-      } else if (playerChoice === "paper") {
-        computerScore++;
-        return `computadora eligio ${computerChoice} y jugador eligio ${playerChoice}. Gana la computadora!`;
-      }
-      break;
-  }
-}
-function game() {
-  resetScore();
-  while (playerScore < 5 && computerScore < 5) {
-    newSelection();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log(`JUGADOR (${playerScore}) - COMPUTADORA (${computerScore})`);
-  }
-  showWinner();
-  resetScore();
+  showRoundWinner(roundWinner);
 }
 
-function newSelection() {
-  playerSelection = prompt("Rock, paper, scissors?").toLowerCase();
-  computerSelection = computerPlay();
+function showRoundWinner(winner) {
+  if (winner === "player") {
+    playerChoice.style.color = "green";
+    computerChoice.style.color = "red";
+    roundWinnerDisplay.textContent = "Player wins this round";
+  } else if (winner === "computer") {
+    playerChoice.style.color = "red";
+    computerChoice.style.color = "green";
+    roundWinnerDisplay.textContent = "Computer wins this round";
+  } else if (winner === "nobody") {
+    playerChoice.style.color = "black";
+    computerChoice.style.color = "black";
+    roundWinnerDisplay.textContent = "Nobody wins this round";
+  }
 }
 
 function showWinner() {
   if (playerScore > computerScore) {
-    console.log(`Gana el JUGADOR (${playerScore} - ${computerScore})`);
+    winnerDisplay.textContent = "Player WINS!";
   } else if (computerScore > playerScore) {
-    console.log(`Gana el JUGADOR (${computerScore} - ${playerScore})`);
-  } else {
-    console.log(`ES UN EMPATE! (${computerScore} - ${playerScore})`);
+    winnerDisplay.textContent = "Computer WINS";
   }
 }
-
-game();
