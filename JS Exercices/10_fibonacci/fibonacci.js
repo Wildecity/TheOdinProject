77bd3dc3ec409975d5a1edb1ca1b52ef50e4792d
Wildecity <81671271+Wildecity@users.noreply.github.com>
2021-10-21 16:31:53 -0300

    Exercices completed

--- JS Exercices/10_fibonacci/fibonacci.js
+++ JS Exercices/10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+  if (count < 0) return "OOPS";
+  if (count === 0) return 0;
+  let a = 0;
+  let b = 1;
+  let sequence = [];
+  for (let i = 0; i < count; i++) {
+    let temp = b + a;
+    a = b;
+    b = temp;
+    sequence.push(a);
+  }
+  console.log("Sequence is");
+  console.table(sequence);
+  return a;
 };
 
 // Do not edit below this line
--- JS Exercices/12_findTheOldest/findTheOldest.js
+++ JS Exercices/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+const findTheOldest = function (array) {
+  return array.reduce((oldest, next) => {
+    let oldestAge = stillAlive(oldest);
+    let nextAge = stillAlive(next);
+    return oldestAge > nextAge ? oldest : next;
+  });
+};
+
+const stillAlive = function (person) {
+  if (person.yearOfDeath) {
+    return person.yearOfDeath - person.yearOfBirth;
+  } else {
+    return currentYear - person.yearOfBirth;
+  }
 };
 
 // Do not edit below this line
